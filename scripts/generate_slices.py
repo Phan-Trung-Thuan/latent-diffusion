708e9c4c59ef7f9d18ccfe8d2df907ce80439f79
Phan Trung Thuận <thuantk000@gmail.com>
2025-11-15 10:01:44 +0700

    update

--- scripts/generate_slices.py
+++ scripts/generate_slices.py
@@ -53,10 +53,9 @@ def splice(left, right):
 # ==================================================
 @torch.no_grad()
 def ddim_step(sampler, x, c, uc, t_index):
-    t = torch.tensor([sampler.ddim_timesteps[t_index]], device=x.device, dtype=x.dtype)
+    t = torch.tensor([sampler.ddim_timesteps[t_index]], device=x.device, dtype=torch.long)
     c = c.to(t.dtype)
     uc = uc.to(t.dtype)
-    print(t)
     x_prev, pred_x0 = sampler.p_sample_ddim(
         x, c, t, index=t_index,
         unconditional_guidance_scale=5.0,
