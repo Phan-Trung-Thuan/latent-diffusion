7dbcb94ddfba0a1ddeaba6e7949651c98f66e032
Phan Trung Thuận <thuantk000@gmail.com>
2025-11-21 18:10:30 +0700

    update

--- scripts/test3.py
+++ scripts/test3.py
@@ -74,11 +74,12 @@ if __name__ == "__main__":
     sampler = DDIMSampler(model)
 
     uc = model.get_learned_conditioning([""])
-    prompts = []
-    prompts.extend(["a beautiful landscape with tree, mountains and sky"] * 3)
-    prompts.extend(["a beautiful landscape with lake, mountains and sky"] * 4)
-    prompts.extend(["a beautiful landscape with grass, mountains and sky"] * 3)
-    c = model.get_learned_conditioning(prompts)
+    # prompts = []
+    # prompts.extend(["a beautiful landscape with tree, mountains and sky"] * 3)
+    # prompts.extend(["a beautiful landscape with lake, mountains and sky"] * 4)
+    # prompts.extend(["a beautiful landscape with grass, mountains and sky"] * 3)
+    # c = model.get_learned_conditioning(prompts)
+    c = model.get_learned_conditioning(["a beautiful landscape with grass, mountains and sky"])
     print(c.shape)
 
     panorama_latent = sampler.lsjd_sample(
