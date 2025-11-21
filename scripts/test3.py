42f399ebfb00a8d88f0ec4f73f6ab1e58e422643
Phan Trung Thuận <thuantk000@gmail.com>
2025-11-21 12:42:14 +0700

    update

--- scripts/test3.py
+++ scripts/test3.py
@@ -82,7 +82,7 @@ if __name__ == "__main__":
         num_slices=10,
         conditioning=c,
         w_swap=1,
-        ref_guided_rate=0.1,
+        ref_guided_rate=0.0,
         overlap_ratio=0.25,
         unconditional_guidance_scale=5,
         unconditional_conditioning=uc
